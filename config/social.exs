4ff1f7054173b1042fe42239e3de561fc7078a49
Mayel de Borniol <git@mayel.space>
2025-04-02 21:07:59 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1140

--- config/social.exs
+++ config/social.exs
@@ -10,6 +10,8 @@ config :bonfire_social, Bonfire.Social.Pins, modularity: :disabled
 config :bonfire_boundaries, Bonfire.UI.Boundaries.SetBoundariesLive, modularity: :disabled
 config :bonfire_boundaries, Bonfire.UI.Boundaries.PreviewBoundariesLive, modularity: :disabled
 
+config :bonfire_ui_boundaries, Bonfire.Boundaries.Locking.LiveHandler, modularity: :disabled
+
 # config :bonfire_ui_reactions, Bonfire.UI.Reactions.BookmarksLive, modularity: :disabled
 # config :bonfire_ui_reactions, Bonfire.UI.Reactions.BookmarkActionLive, modularity: :disabled
 
