ae86c029db4ba17f45d18955cda2bfd64c85c93b
ivan <ivanminutillo@outlook.it>
2025-04-02 18:57:18 +0200

    disabled modules

--- config/social.exs
+++ config/social.exs
@@ -15,6 +15,7 @@ config :bonfire_boundaries, Bonfire.UI.Boundaries.PreviewBoundariesLive, modular
 
 config :bonfire_ui_social, Bonfire.Social.Pins, modularity: :disabled
 config :bonfire_label, Bonfire.Label.Web.ContentLabelLive, modularity: :disabled
+config :bonfire_ui_reactions, Bonfire.UI.Reactions.PinActionLive, modularity: :disabled
 
 config :bonfire, :ui,
   default_nav_extensions: [
