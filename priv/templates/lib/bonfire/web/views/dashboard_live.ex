10762aa17594f2d266cfa5e54e99e332062b0fd8
ivan <ivanminutillo@outlook.it>
2026-05-13 22:08:09 +0200

    Update dashboard_live.ex

--- priv/templates/lib/bonfire/web/views/dashboard_live.ex
+++ priv/templates/lib/bonfire/web/views/dashboard_live.ex
@@ -51,6 +51,14 @@ defmodule Bonfire.Web.Views.DashboardLive do
                 true,
                 current_user: current_user
               ) && {Bonfire.Tag.Web.WidgetTagsLive, []},
+              current_user &&
+                Settings.get(
+                  [Bonfire.Web.Views.DashboardLive, :include, :recent_articles],
+                  true,
+                  current_user: current_user
+                ) &&
+                {Bonfire.UI.Social.WidgetRecentArticlesLive,
+                 [limit: 5, widget_title: l("Recent Articles"), image_position: :bottom]},
               # {Bonfire.UI.Social.WidgetTrendingLinksLive, []},
               # Settings.get([Bonfire.Web.Views.DashboardLive, :include, :admins], true,
               #   current_user: current_user
@@ -91,7 +99,7 @@ defmodule Bonfire.Web.Views.DashboardLive do
               true,
               current_user: current_user
             ) &&
-            {Bonfire.UI.Social.WidgetSuggestedProfilesLive, [widget_title: l("Who to follow")]},
+            {Bonfire.UI.Social.WidgetSuggestedProfilesLive, [widget_title: l("Who to follow")]}
           # Settings.get(
           #   [Bonfire.Web.Views.DashboardLive, :include, :trending_links],
           #   true,
@@ -99,14 +107,6 @@ defmodule Bonfire.Web.Views.DashboardLive do
           # ) &&
           #   {Bonfire.UI.Social.WidgetTrendingLinksLive,
           #    [limit: 5, widget_title: l("Trending Links")]},
-          current_user &&
-            Settings.get(
-              [Bonfire.Web.Views.DashboardLive, :include, :recent_articles],
-              true,
-              current_user: current_user
-            ) &&
-            {Bonfire.UI.Social.WidgetRecentArticlesLive,
-             [limit: 5, widget_title: l("Recent Articles")]}
         ],
         & &1
       )
